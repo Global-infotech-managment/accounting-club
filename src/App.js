bafda467a2ff514c4f782914e8ba00e2afdc0477
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-01-25 12:39:14 +0530

    add reportWebVitals

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className="text-[200px] text-red-600">Accounting-club</p>
+    </>
+  )
+}
+
+export default App
