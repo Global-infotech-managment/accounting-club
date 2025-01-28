a3aeaffd813cc6d49db5ceb9673e4b261b42f92b
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-01-28 11:14:27 +0530

    fav icon change

--- src/components/OnlineCourse.jsx
+++ src/components/OnlineCourse.jsx
@@ -8,12 +8,20 @@ import { Link } from 'react-router-dom'
 import { onlineCourse } from '../utils/helper'
 import OnlineBook from '../assets/images/png/online-book.png'
 import { EffectFade, Autoplay } from 'swiper/modules'
+import Button from './common/Button'
+
 const OnlineCourse = () => {
   const swiperRef = useRef(null)
   const [activeIndex, setActiveIndex] = useState(0)
+  const [isShowMore, setIsShowMore] = useState(false)
+
   const handleDotClick = (index) => {
     swiperRef.current?.swiper.slideTo(index)
     setActiveIndex(index)
+  }
+
+  const toggleShowMore = () => {
+    setIsShowMore((prev) => !prev)
   }
 
   return (
@@ -28,26 +36,14 @@ const OnlineCourse = () => {
           className="mb-4 mt-3 sm:mt-4 md:mb-6 lg:mx-auto lg:mb-10 lg:max-w-[619px] lg:text-center"
           text="Be an Accounting and taxation expert in few days even if you don't have any Accounting background. Choose a program as per your requirement"
         />
-        <Swiper
-          onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-          breakpoints={{
-            1024: { slidesPerView: 3, spaceBetween: 20 },
-            768: { slidesPerView: 2, spaceBetween: 16 },
-            320: { slidesPerView: 1, spaceBetween: 10 },
-          }}
-          ref={swiperRef}
-          modules={[EffectFade, Autoplay]}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          slidesPerView={1}
-          fadeEffect={{ crossFade: true }}
-        >
-          {onlineCourse.map((obj, index) => (
-            <SwiperSlide key={index}>
-              <div className="rounded-5 border border-black border-opacity-5 bg-white p-4 max-sm:min-h-[208px] sm:p-6 md:min-h-[231px] lg:min-h-[246px] xl:min-h-[224px]">
-                <h2 className="flex items-center gap-2 text-xl font-semibold leading-150">
+        {isShowMore ? (
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            {onlineCourse.map((obj, index) => (
+              <div
+                key={index}
+                className="rounded-5 border border-black border-opacity-5 bg-white p-4 max-sm:min-h-[208px] sm:p-6 md:min-h-[231px] lg:min-h-[246px] xl:min-h-[224px]"
+              >
+                <h2 className="leading-150 flex items-center gap-2 text-xl font-semibold">
                   <Icons iconName={obj.iconName} />
                   {obj.title}
                 </h2>
@@ -69,17 +65,70 @@ const OnlineCourse = () => {
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <div className="custom-pagination mt-4 flex w-full justify-center sm:mt-6 md:!mt-10">
-          {onlineCourse.map((_, index) => (
-            <span
-              key={index}
-              className={`dot ${activeIndex === index ? 'dot-active' : ''}`}
-              onClick={() => handleDotClick(index)}
-            ></span>
-          ))}
+            ))}
+          </div>
+        ) : (
+          <Swiper
+            onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+            breakpoints={{
+              1024: { slidesPerView: 3, spaceBetween: 20 },
+              768: { slidesPerView: 2, spaceBetween: 16 },
+              320: { slidesPerView: 1, spaceBetween: 10 },
+            }}
+            ref={swiperRef}
+            modules={[EffectFade, Autoplay]}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            slidesPerView={1}
+            fadeEffect={{ crossFade: true }}
+          >
+            {onlineCourse.map((obj, index) => (
+              <SwiperSlide key={index}>
+                <div className="rounded-5 border border-black border-opacity-5 bg-white p-4 max-sm:min-h-[208px] sm:p-6 md:min-h-[231px] lg:min-h-[246px] xl:min-h-[224px]">
+                  <h2 className="leading-150 flex items-center gap-2 text-xl font-semibold">
+                    <Icons iconName={obj.iconName} />
+                    {obj.title}
+                  </h2>
+                  <div className="mt-2 flex items-start gap-5">
+                    <img
+                      width={23}
+                      height={84}
+                      src={obj.levelImg}
+                      alt={obj.title}
+                    />
+                    <div>
+                      <Paragraph
+                        className="mb-3 lg:max-w-[274px]"
+                        text={obj.desc}
+                      />
+                      <Link className="text-[#3574E1] underline" to={'/'}>
+                        {obj.readMore}
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+        {/* dots */}
+        {/* <div className="custom-pagination mt-4 flex w-full justify-center sm:mt-6 md:!mt-10">
+          {!isShowMore &&
+            onlineCourse.map((_, index) => (
+              <span
+                key={index}
+                className={`dot ${activeIndex === index ? 'dot-active' : ''}`}
+                onClick={() => handleDotClick(index)}
+              ></span>
+            ))}
+        </div> */}
+        <div className="mt-6 flex items-center justify-center text-center">
+          <Button
+            onClick={toggleShowMore}
+            bgBtn={isShowMore ? 'Show Less' : 'Show More'}
+          />
         </div>
       </div>
       <img
--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Button = ({ bgBtn, transparentBtn, path, className }) => {
+const Button = ({ bgBtn, transparentBtn, path, className, onClick }) => {
   return (
     <>
       {path ? (
         <Link
-          className={`${className} flex h-12 items-center justify-center rounded-1 px-6 py-2 font-medium !leading-[133%] duration-300 sm:h-[52px] sm:py-3 sm:text-lg ${bgBtn ? 'bg-primary text-white hover:border hover:!border-primary hover:!bg-white hover:!text-primary' : 'border border-primary bg-white text-primary hover:!border-primary hover:!bg-primary hover:!text-white'}`}
+          className={`${className} rounded-1 flex h-12 items-center justify-center px-6 py-2 font-medium !leading-[133%] duration-300 sm:h-[52px] sm:py-3 sm:text-lg ${bgBtn ? 'bg-primary text-white hover:border hover:!border-primary hover:!bg-white hover:!text-primary' : 'border border-primary bg-white text-primary hover:!border-primary hover:!bg-primary hover:!text-white'}`}
           to={path}
         >
           {transparentBtn}
@@ -14,7 +14,8 @@ const Button = ({ bgBtn, transparentBtn, path, className }) => {
         </Link>
       ) : (
         <button
-          className={`${className} flex h-12 items-center justify-center rounded-1 border border-[transparent] px-6 py-2 font-medium !leading-[133%] duration-300 sm:h-[52px] sm:py-[14px] sm:text-lg ${bgBtn ? 'bg-primary text-white hover:border-primary hover:bg-white hover:text-primary' : 'border-primary bg-white text-primary hover:border-primary hover:bg-primary hover:text-white'}`}
+          onClick={onClick}
+          className={`${className} rounded-1 flex h-12 items-center justify-center border border-[transparent] px-6 py-2 font-medium !leading-[133%] duration-300 sm:h-[52px] sm:py-[14px] sm:text-lg ${bgBtn ? 'bg-primary text-white hover:border-primary hover:bg-white hover:text-primary' : 'border-primary bg-white text-primary hover:border-primary hover:bg-primary hover:text-white'}`}
         >
           {transparentBtn}
           {bgBtn}
