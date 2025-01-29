444bd12beca5faf77fb695020b49d6926360b00d
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-01-29 15:14:10 +0530

    button error solve

--- src/components/JourneyStart.jsx
+++ src/components/JourneyStart.jsx
@@ -3,27 +3,34 @@ import Heading from './common/Heading'
 import Paragraph from './common/Paragraph'
 import Button from './common/Button'
 import FooterEllipse from '../assets/images/png/footer-ellipse.png'
+import { STUDENT_LOGIN_ROUTE, STUDENT_SIGNUP_ROUTE } from '../utils/constant'
 
 const JourneyStart = () => {
   return (
-    <div className="relative w-full pt-16 md:pt-24 lg:pt-[120px]" id="admission">
+    <div
+      className="relative w-full pt-16 md:pt-24 lg:pt-[120px]"
+      id="admission"
+    >
       <div className="container relative z-20 px-3 lg:max-w-[1184px]">
-        <div className="bg-journey bg-center bg-no-repeat p-6 max-sm:rounded-4 sm:bg-[length:100%_100%] sm:p-8  lg:p-16">
+        <div className="max-sm:rounded-4 bg-journey bg-center bg-no-repeat p-6 sm:bg-[length:100%_100%] sm:p-8 lg:p-16">
           <div className="mx-auto w-full max-w-[746px]">
             <Heading
               className="text-center !text-white"
-              middleText="Start Your Journey"/>
+              middleText="Start Your Journey"
+            />
             <Paragraph
               className="mt-2 text-center !text-white md:mt-4"
               text="Accountants Club transformed my career with practical training. The expert guidance and unique techniques made learning accounting effortless and enjoyable!"
             />
             <div className="mt-3 flex items-center justify-center gap-2 max-sm:flex-wrap sm:mt-4 sm:gap-3 md:mt-6 md:gap-5 lg:mt-10">
               <Button
+                path={STUDENT_LOGIN_ROUTE}
                 className="!bg-white !text-primary hover:!border-white hover:!bg-[transparent] hover:!text-white max-sm:w-full"
                 bgBtn="Get Started"
               />
               <Button
                 className="!border-white !bg-primary bg-[transparent] text-white hover:!bg-white hover:!text-primary max-sm:w-full"
+                path={STUDENT_SIGNUP_ROUTE}
                 transparentBtn="Register For Free"
               />
             </div>
--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ bgBtn, transparentBtn, path, className, onClick }) => {
     <>
       {path ? (
         <Link
-          className={`${className} rounded-1 flex h-12 items-center justify-center px-6 py-2 font-medium !leading-[133%] duration-300 sm:h-[52px] sm:py-3 sm:text-lg ${bgBtn ? 'bg-primary text-white hover:border hover:!border-primary hover:!bg-white hover:!text-primary' : 'border border-primary bg-white text-primary hover:!border-primary hover:!bg-primary hover:!text-white'}`}
+          className={`${className} rounded-1 flex h-12 items-center justify-center border border-[transparent] px-6 py-2 font-medium !leading-[133%] duration-300 sm:h-[52px] sm:py-[14px] sm:text-lg ${bgBtn ? 'bg-primary text-white hover:border-primary hover:bg-white hover:text-primary' : 'border-primary bg-white text-primary hover:border-primary hover:bg-primary hover:text-white'}`}
           to={path}
         >
           {transparentBtn}
