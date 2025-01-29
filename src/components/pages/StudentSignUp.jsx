77deed633e6ad7ed97fe468d1cee907bfc53128b
Sumit chouhan <sumitkhan.dhana@gmail.com>
2025-01-29 18:18:45 +0530

    code merge

--- src/components/pages/StudentSignUp.jsx
+++ src/components/pages/StudentSignUp.jsx
@@ -39,106 +39,137 @@ const StudentSignUp = () => {
             className="mx-auto mb-6 max-w-[590px] text-center text-black"
             text="Register today to access your free CA course and start learning anytime, anywhere with expert guidance!"
           />
-          <div className="grid grid-cols-2 gap-4">
-            <Input
-              placeholder="Name*"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <Input
-              placeholder="Age*"
-              type="number"
-              value={age}
-              onChange={(e) => setAge(e.target.value)}
-            />
-            <Input
-              placeholder="Qualification*"
-              type="text"
-              value={qualification}
-              onChange={(e) => setQualification(e.target.value)}
-            />
-            <Dropdown
-              label="Experience if any*"
-              options={[
-                { label: 'Fresher', value: 'fresher' },
-                { label: '1 year', value: '1' },
-                { label: '2 year', value: '2' },
-                { label: '3 year', value: '3' },
-                { label: '4 year', value: '4' },
-                { label: '5 year+', value: '5' },
-              ]}
-              onChange={setExperience}
-              value={experience}
-            />
-            <Input
-              placeholder="Email Id*"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              placeholder="Mobile Number*"
-              type="tel"
-              value={mobile}
-              onChange={(e) => setMobile(e.target.value)}
-            />
-            <Input
-              placeholder="Postal Address* [For study Material] Line 1"
-              type="text"
-              value={addressLine1}
-              onChange={(e) => setAddressLine1(e.target.value)}
-            />
-            <Input
-              placeholder="Line 2"
-              type="text"
-              value={addressLine2}
-              onChange={(e) => setAddressLine2(e.target.value)}
-            />
-            <Input
-              placeholder="City*"
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-            <Input
-              placeholder="State*"
-              type="text"
-              value={state}
-              onChange={(e) => setState(e.target.value)}
-            />
-            <Dropdown
-              label="Country*"
-              options={[
-                { label: 'America', value: 'America' },
-                { label: 'Afghanistan', value: 'Afghanistan' },
-                { label: 'Albania', value: 'Albania' },
-                { label: 'Andorra', value: 'Andorra' },
-                { label: 'Antigua and Barbuda', value: 'Antigua and Barbuda' },
-                { label: 'Argentina', value: 'Argentina' },
-              ]}
-              onChange={setCountry}
-              value={country}
-            />
-            <Input
-              placeholder="Pin Code*"
-              type="text"
-              value={pinCode}
-              onChange={(e) => setPinCode(e.target.value)}
-            />
-            <Input
-              placeholder="Password*"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Input
-              placeholder="Retype Password*"
-              type="password"
-              value={retypePassword}
-              onChange={(e) => setRetypePassword(e.target.value)}
-            />
-            <div className="col-span-2 flex items-center">
+          <div className="flex flex-wrap">
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Name*"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Age*"
+                type="number"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Qualification*"
+                type="text"
+                value={qualification}
+                onChange={(e) => setQualification(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Dropdown
+                label="Experience if any*"
+                options={[
+                  { label: 'Fresher', value: 'fresher' },
+                  { label: '1 year', value: '1' },
+                  { label: '2 year', value: '2' },
+                  { label: '3 year', value: '3' },
+                  { label: '4 year', value: '4' },
+                  { label: '5 year+', value: '5' },
+                ]}
+                onChange={setExperience}
+                value={experience}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Email Id*"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Mobile Number*"
+                type="tel"
+                value={mobile}
+                onChange={(e) => setMobile(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Postal Address* [For study Material] Line 1"
+                type="text"
+                value={addressLine1}
+                onChange={(e) => setAddressLine1(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Line 2"
+                type="text"
+                value={addressLine2}
+                onChange={(e) => setAddressLine2(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="City*"
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="State*"
+                type="text"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Dropdown
+                label="Country*"
+                options={[
+                  { label: 'America', value: 'America' },
+                  { label: 'Afghanistan', value: 'Afghanistan' },
+                  { label: 'Albania', value: 'Albania' },
+                  { label: 'Andorra', value: 'Andorra' },
+                  {
+                    label: 'Antigua and Barbuda',
+                    value: 'Antigua and Barbuda',
+                  },
+                  { label: 'Argentina', value: 'Argentina' },
+                ]}
+                onChange={setCountry}
+                value={country}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Pin Code*"
+                type="text"
+                value={pinCode}
+                onChange={(e) => setPinCode(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Password*"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="w-full sm:w-6/12 px-2 py-3">
+              <Input
+                placeholder="Retype Password*"
+                type="password"
+                value={retypePassword}
+                onChange={(e) => setRetypePassword(e.target.value)}
+              />
+            </div>
+            <div className="flex w-full items-center sm:w-6/12">
               <input
                 type="checkbox"
                 id="terms"
@@ -154,14 +185,15 @@ const StudentSignUp = () => {
               </label>
             </div>
           </div>
+
           <Button
             className="mt-4 w-full md:mt-6 lg:mt-10"
             transparentBtn="Read more"
           />
-          <div className="col-span-2 justify-center flex items-center pt-7">
+          <div className="col-span-2 flex items-center justify-center pt-7">
             <label htmlFor="terms" className="ml-2">
               Already have an account?
-              <a href="#" className="text-blue-500 font-semibold ps-1">
+              <a href="#" className="text-blue-500 ps-1 font-semibold">
                 Sign In
               </a>
             </label>
