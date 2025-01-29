89153f47e40e30a0313bfdac18f215e855f6cfb7
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-01-29 13:36:00 +0530

    common input add

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
-const Input = () => {
-  return <div>Input</div>
+const Input = ({
+  placeholder,
+  type,
+  value,
+  onChange,
+  mainClassName,
+  labelCLassName,
+  inputClassName,
+}) => {
+  return (
+    <>
+      <div className={`flex w-full flex-col ${mainClassName}`}>
+        <label
+          htmlFor={placeholder}
+          className={`mb-[2px] text-sm text-black ${labelCLassName}`}
+        >
+          {placeholder} <span className="text-orange-red">*</span>
+        </label>
+        <input
+          type={type}
+          className={`rounded-[4px] border border-[#4E4E4E1A] bg-[#FBFBFB80] p-2 focus-visible:outline-[1px] focus-visible:outline-orange-red ${inputClassName}`}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    </>
+  )
 }
 
 export default Input
--- src/components/pages/StudentSignUp.jsx
+++ src/components/pages/StudentSignUp.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Input from '../common/Input'
 
 const StudentSignUp = () => {
+  const [inputValue, setInputValue] = useState('')
+  const nameValue = (e) => {
+    setInputValue(e.target.value)
+  }
   return (
-    <div>StudentSignUp</div>
+    <div className="container px-3 lg:max-w-[1184px]">
+      <Input
+        placeholder={'Name'}
+        type={'text'}
+        value={inputValue}
+        onChange={nameValue}
+      />
+    </div>
   )
 }
 
