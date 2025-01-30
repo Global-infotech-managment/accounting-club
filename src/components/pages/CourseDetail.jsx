49e61cebbdedff12da070cf1de23a4a479fbf725
Sumit chouhan <sumitkhan.dhana@gmail.com>
2025-01-30 16:51:56 +0530

    course page in progress

--- src/components/pages/CourseDetail.jsx
+++ src/components/pages/CourseDetail.jsx
@@ -3,11 +3,12 @@ import courseImage from '../../assets/images/png/course-details-hero.png'
 import Heading from '../common/Heading'
 import Paragraph from '../common/Paragraph'
 import Icons from '../common/Icons'
+import Button from '../common/Button'
 const CourseDetail = () => {
   return (
     <div className="container max-w-[1184px] pt-20">
       <div className="flex flex-wrap">
-        <div className="w-8/12">
+        <div className="w-8/12 pe-12">
           <img
             className="w-full object-cover"
             src={courseImage}
@@ -31,29 +32,120 @@ const CourseDetail = () => {
             text="From understanding GST applicability to mastering tax calculations and filing accurate returns, this course ensures a step-by-step approach to learning. Explore key topics like GST rates, e-way bills, reverse charge mechanisms, and common filing errors. By the end of the course, you'll be equipped with the knowledge and skills needed to navigate the GST system seamlessly and stay compliant with tax regulations."
           />
         </div>
-        <div className="w-4/12 ps-12">
+        <div className="w-4/12">
           <div className="flex items-end justify-between">
             <div className="flex items-end">
               <Heading className="!text-black" middleText="50.02" />
               <span className="ps-1 font-semibold">USD</span>
             </div>
+            <Paragraph className="!text-red-500" text="40% Off" />
           </div>
-          <Paragraph className="!text-red-500" text="40% Off" />
-          <div className="flex rounded-[10px] border border-black border-opacity-20 px-5">
-            <div className="flex w-6/12 gap-2.5 border-r border-black border-opacity-20">
+          <div className="mt-2 flex rounded-[10px] border border-black border-opacity-20 px-5">
+            <div className="flex w-6/12 items-center gap-2.5 border-r border-black border-opacity-20">
               <Icons iconName="bookImage" className="size-4" />
-              <div>
-                <Paragraph className="!text-red-500" text="Lessons" />
+              <div className="py-2.5">
+                <Paragraph
+                  className="!text-red-500 opacity-80"
+                  text="Lessons"
+                />
                 <Paragraph className="!text-red-500" text="15" />
               </div>
             </div>
-            <div className="flex w-6/12 gap-2.5">
+            <div className="ms-5 flex w-6/12 items-center gap-2.5">
               <Icons iconName="difficulity" className="size-4" />
-              <div>
-                <Paragraph className="!text-red-500" text="Difficulity" />
+              <div className="py-2.5">
+                <Paragraph
+                  className="!text-red-500 opacity-80"
+                  text="Difficulity"
+                />
                 <Paragraph className="!text-red-500" text="Moderate" />
               </div>
             </div>
+          </div>
+          <div className="mt-3 flex items-center gap-2.5">
+            <Icons iconName="students" className="size-4" />
+            <Paragraph
+              className="!text-red-500 !text-lg"
+              text={
+                <>
+                  <span className="pe-1 !opacity-80">Students:</span>
+                  <span className="font-medium">4,000</span>
+                </>
+              }
+            />
+          </div>
+          <div className="mt-3 flex items-center gap-2.5">
+            <Icons iconName="language" className="size-4" />
+            <Paragraph
+              className="!text-red-500 !text-lg"
+              text={
+                <>
+                  <span className="pe-1 !opacity-80">Language:</span>
+                  <span className="font-medium">English</span>
+                </>
+              }
+            />
+          </div>
+          <div className="mt-3 flex items-center gap-2.5">
+            <Icons iconName="subtitle" className="size-4" />
+            <Paragraph
+              className="!text-red-500 !text-lg"
+              text={
+                <>
+                  <span className="pe-1 !opacity-80">Subtitles:</span>
+                  <span className="font-medium">
+                    English, Hindi, French, Italian, Russian, Polish, Dutch
+                  </span>
+                </>
+              }
+            />
+          </div>
+          <div className="mt-3 flex items-center gap-2.5">
+            <Icons iconName="duration" className="size-4" />
+            <Paragraph
+              className="!text-red-500 !text-lg"
+              text={
+                <>
+                  <span className="pe-1 !opacity-80">Duration:</span>
+                  <span className="font-medium"> 15h 32min</span>
+                </>
+              }
+            />
+          </div>
+          <div className="mt-3 flex items-center gap-2.5">
+            <Icons iconName="resources" className="size-4" />
+            <Paragraph
+              className="!text-red-500 !text-lg"
+              text={
+                <>
+                  <span className="pe-1 !opacity-80">
+                    Additional Resources:
+                  </span>
+                  <span className="font-medium">12 Files</span>
+                </>
+              }
+            />
+          </div>
+          <div className="mt-3 flex items-center gap-2.5">
+            <Icons iconName="certificate" className="size-4" />
+            <Paragraph
+              className="!text-red-500 !text-lg"
+              text={
+                <>
+                  <span className="pe-1 !opacity-80">Certificate:</span>
+                  <span className="font-medium">
+                    {' '}
+                    Upon completion of the courses
+                  </span>
+                </>
+              }
+            />
+          </div>
+          <div className='pt-8 mt-6 border-t border-black border-opacity-10'>
+            <Button
+              className="max-sm:px-3 max-sm:py-2 max-sm:text-center w-full !rounded-xl"
+              bgBtn="Enroll Now"
+            />
           </div>
         </div>
       </div>
