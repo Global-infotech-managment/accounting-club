6b540af39278e351d7df9f34bc80184eeaf5c940
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-01-31 18:09:15 +0530

    slug trying

--- src/components/pages/student/dashboard/Dashboard.jsx
+++ src/components/pages/student/dashboard/Dashboard.jsx
@@ -1,5 +1,4 @@
 import { useLocation, useParams } from 'react-router-dom'
-import EnrolledCourseDetail from './EnrolledCourseDetail'
 import DashboardNav from '../../../common/DashboardNav'
 import StudentSidebar from '../StudentSidebar'
 import DashboardUi from './DashboardUi'
@@ -13,7 +12,6 @@ import NewsAndBlogs from './NewsAndBlogs'
 const Dashboard = () => {
   const location = useLocation()
   const queryParams = new URLSearchParams(location.search)
-  const { slug } = useParams() // Extract slug dynamically
 
   let activeSidebar = queryParams.get('activeSidebar') || 'dashboard'
   activeSidebar = activeSidebar.replace(/~/g, '-')
@@ -28,19 +26,15 @@ const Dashboard = () => {
 
         <div className="flex-grow overflow-auto">
           <div className="min-h-full p-2 sm:p-4 md:p-6 lg:px-10 lg:py-8">
-            {slug ? (
-              <EnrolledCourseDetail />
-            ) : (
-              <>
-                {activeSidebar === 'dashboard' && <DashboardUi />}
-                {activeSidebar === 'my-course' && <MyCoursesUi />}
-                {activeSidebar === 'my-certificate' && <MyCertificateUi />}
-                {activeSidebar === 'job-placement' && <JobPlacement />}
-                {activeSidebar === 'job-work' && <JobWork />}
-                {activeSidebar === 'support' && <Support />}
-                {activeSidebar === 'news---blog' && <NewsAndBlogs />}
-              </>
-            )}
+            <>
+              {activeSidebar === 'dashboard' && <DashboardUi />}
+              {activeSidebar === 'my-course' && <MyCoursesUi />}
+              {activeSidebar === 'my-certificate' && <MyCertificateUi />}
+              {activeSidebar === 'job-placement' && <JobPlacement />}
+              {activeSidebar === 'job-work' && <JobWork />}
+              {activeSidebar === 'support' && <Support />}
+              {activeSidebar === 'news---blog' && <NewsAndBlogs />}
+            </>
           </div>
         </div>
       </div>
