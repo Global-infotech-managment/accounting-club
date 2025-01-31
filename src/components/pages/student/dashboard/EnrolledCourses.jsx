d754a965c8b31bdd329bfa20a1c9bfdcf33c2934
Sumit chouhan <sumitkhan.dhana@gmail.com>
2025-01-31 17:03:06 +0530

    add heading and description

--- src/components/pages/student/dashboard/EnrolledCourses.jsx
+++ src/components/pages/student/dashboard/EnrolledCourses.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
 import EnrolledCoursesCard from './EnrolledCoursesCard'
 import { enrollCourses } from '../../../../utils/helper'
+import Heading from '../../../common/Heading'
+import Paragraph from '../../../common/Paragraph'
 
 
 
 const EnrolledCourses = () => {
   return (
-    <div className="flex flex-wrap">
-      {enrollCourses.map((course) => (
-        <div key={course.id} className="px-3 pt-6 sm:w-6/12 xl:w-4/12">
-          <EnrolledCoursesCard
-            image={course.image}
-            heading={course.heading}
-            description={course.description}
-          />
-        </div>
-      ))}
+    <div className="container max-w-[1920px] pt-2 px-2">
+      <Heading
+        className="sm:!text-2xl mb-1 text-xl !text-black"
+        middleText="My Enrolled Courses"
+      />
+      <Paragraph
+        className="!text-sm !text-black opacity-80"
+        text="Access and manage your enrolled courses for seamless learning anytime, anywhere."
+      />
+      <div className="mt-4 grid gap-5 sm:grid-cols-2 xl:grid-cols-3">
+        {enrollCourses.map((course) => (
+          <div key={course.id}>
+            <EnrolledCoursesCard
+              image={course.image}
+              heading={course.heading}
+              description={course.description}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
