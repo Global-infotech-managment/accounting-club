3aa29e8b4f1b0326cc382c71de5537d0f6797f86
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-02-01 13:05:39 +0530

    detailed video add

--- src/components/pages/student/dashboard/EnrolledCourseDetail.jsx
+++ src/components/pages/student/dashboard/EnrolledCourseDetail.jsx
@@ -1,14 +1,33 @@
-import { useParams } from 'react-router-dom'
+import React from 'react'
+import { Routes, Route, useLocation, useParams } from 'react-router-dom'
+import DashboardNav from '../../../common/DashboardNav'
+import StudentCourseDetailSidebar from './StudentCourseDetailSidebar'
+import StudentCourseDetailVideo from './StudentCourseDetailVideo'
 
 const EnrolledCourseDetail = () => {
-  const { slug } = useParams()
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  let activeSidebar = queryParams.get('activeSidebar') || 'dashboard'
+
+  // Normalize sidebar key
+  activeSidebar = activeSidebar.replace(/~/g, '-')
 
   return (
-    <div>
-      <h1>Enrolled Course Detail</h1>
-      <p>
-        Showing details for course: <strong>{slug}</strong>
-      </p>
+    <div className="flex h-screen flex-col">
+      <DashboardNav />
+      <div className="flex flex-grow overflow-hidden">
+        {/* Sidebar */}
+        <div className="h-full lg:w-3/12 lg:max-w-[400px]">
+          <StudentCourseDetailSidebar />
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-grow overflow-auto">
+          <div className="min-h-full p-2 sm:p-4 md:p-6 lg:px-10 lg:py-8">
+            <StudentCourseDetailVideo />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/pages/student/dashboard/StudentCourseDetailSidebar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const StudentCourseDetailSidebar = () => {
+  return <div>StudentCourseDetailSidebar</div>
+}
+
+export default StudentCourseDetailSidebar
