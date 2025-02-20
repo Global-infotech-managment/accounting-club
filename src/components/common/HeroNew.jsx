61e7c77e63e1f39ab87e7dbb664f3c6ce5e8c8fb
Abhimal Group <apple@MacBook-Pro.local>
2025-02-20 14:49:24 +0530

    hero section add

--- src/components/common/HeroNew.jsx
+++ src/components/common/HeroNew.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import Heading from './Heading'
 import Button from './Button'
-
+import circle from '../../assets/images/webp/hero-circle.png'
+import Icons from './Icons'
 const HeroNew = () => {
   return (
-    <div className="bg-hero bg-cover bg-no-repeat pt-12 sm:pt-16 md:pt-24 lg:pt-[129px]">
-      <div className="container w-full px-3 lg:max-w-[1184px]">
-        <div className="flex flex-wrap">
-          <div className="w-6/12">
+    <div className="bg-hero flex min-h-[700px] items-center bg-cover bg-no-repeat py-10 pb-10 lg:mt-[-90px] lg:min-h-screen lg:pb-0">
+      <div className="container flex h-full w-full px-3 lg:max-w-[1184px]">
+        <div className="flex flex-col-reverse flex-wrap lg:flex-row">
+          <div className="relative flex w-full flex-col items-start justify-center pt-5 lg:w-6/12 lg:pt-0">
+            <div className="absolute right-[50%] top-[80%] hidden max-w-[63px] rotate-45 sm:top-[70%] md:right-[60%] lg:right-[10%] lg:top-[6%] lg:block lg:rotate-0">
+              <Icons iconName={'heroArrow'} />
+            </div>
             <Heading
               className={
-                '!text-custom-5xl font-bold !leading-[100%] !text-white'
+                'xl:!text-custom-5xl !text-[35px] font-bold !leading-[100%] !text-white sm:!text-[40px] md:!text-[50px]'
               }
               middleText={'Elevate Your Accounting Skills'}
             />
-            <p className="mt-3 text-[16px] font-normal text-white opacity-80 border-l-2 ps-3">
+            <p className="mt-3 border-l-2 ps-3 text-[14px] font-normal text-white opacity-80 md:text-[16px]">
               Boost your accounting career with expert-led courses designed to
               sharpen your skills, enhance your knowledge, and unlock new
               professional opportunities in the accounting field.
             </p>
-            <Button className={"mt-10"} bgWhite={'Start Learning Now'} />
+            <Button
+              className={'relative z-10 mt-5 lg:mt-10'}
+              bgWhite={'Start Learning Now'}
+            />
+          </div>
+          <div className="flex w-full items-center justify-center lg:w-6/12">
+            <img
+              src={circle}
+              alt="circle"
+              className="w-full max-w-[400px] lg:max-w-[448px]"
+            />
           </div>
         </div>
-        HeroNew
       </div>
     </div>
   )
