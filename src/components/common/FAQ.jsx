c98c48643c61c75e8728de721858c63be531d5e2
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-02-28 16:24:36 +0530

    faq heading add

---
+++ src/components/common/FAQ.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import faqLine from '../../assets/images/svg/faqLine.svg'
+const FAQ = () => {
+  return (
+    <div className="container mt-[110px] w-full px-3 lg:max-w-[1184px]">
+      <p className="mb-4 text-center text-[30px] font-bold leading-tight text-black sm:text-[35px] md:text-[40px] lg:text-[48px]">
+        Frequently Asked <span className="text-gradient">Questions</span>
+      </p>
+      <p className="mx-auto max-w-[611px] text-center text-[14px] font-normal text-black md:text-[16px]">
+        Clear and Concise Answers to Common Queries for Better Understanding and
+        Hassle-Free Experience.
+      </p>
+      <img
+        src={faqLine}
+        alt="faq line"
+        className="mx-auto w-full max-w-[394px]"
+      />
+      <div className="mt-[51px] flex flex-wrap">
+        <div className="w-7/12"></div>
+      </div>
+    </div>
+  )
+}
+
+export default FAQ
--- src/components/pages/CoursesPage.jsx
+++ src/components/pages/CoursesPage.jsx
@@ -3,6 +3,7 @@ import OnlineCoursesSell from '../OnlineCoursesSell'
 import HeroNew from '../common/HeroNew'
 import TrustedBy from '../common/TrustedBy'
 import BestFacilities from '../common/BestFacilities'
+import FAQ from '../common/FAQ'
 
 const CoursesPage = () => {
   return (
@@ -11,6 +12,7 @@ const CoursesPage = () => {
       <TrustedBy />
       <OnlineCoursesSell />
       <BestFacilities />
+      <FAQ />
     </>
   )
 }
