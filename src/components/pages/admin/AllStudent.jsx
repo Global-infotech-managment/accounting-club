131e0b189dee4392c68081bf214703cfb2c26cf0
sukhvinder-singh-166 <monudhaka829@gmail.com>
2025-03-04 18:35:41 +0530

    student array add in admin

--- src/components/pages/admin/AllStudent.jsx
+++ src/components/pages/admin/AllStudent.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../common/Button'
+import { studentList } from '../../../utils/helper'
+
+const itemsPerPage = 6
 
 const AllStudent = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const totalPages = Math.ceil(studentList.length / itemsPerPage)
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const currentData = studentList.slice(startIndex, startIndex + itemsPerPage)
+
   return (
-    <div>AllStudent</div>
+    <div className="overflow-x-auto md:p-4">
+      <table className="border-gray-200 shadow-md min-w-full overflow-hidden rounded-xl border bg-white">
+        <thead className="bg-gray-100">
+          <tr>
+            {['Sr. No', 'Date', 'Name', 'Email', 'State', 'PinCode', ,].map(
+              (header, index) => (
+                <th
+                  key={index}
+                  className="text-sm text-nowrap border border-[#FFFFFF33] bg-[#253466] px-4 py-2 text-center font-semibold text-white"
+                >
+                  {header}
+                </th>
+              )
+            )}
+          </tr>
+        </thead>
+        <tbody className="bg-[#F7F7F7]">
+          {currentData.map((item, index) => (
+            <tr
+              key={index}
+              className="hover:bg-gray-50 text-nowrap border-t bg-[#F7F7F7] text-center"
+            >
+              <td className="border border-[#D7D7D7] px-4 py-2">
+                {index < 9 && '0'}
+                {index + 1}
+              </td>
+              <td className="border border-[#D7D7D7] px-4 py-2">{item.date}</td>
+              <td className="border border-[#D7D7D7] px-4 py-2">{item.name}</td>
+              <td className="border border-[#D7D7D7] px-4 py-2 text-left">
+                {item.email}
+              </td>
+              <td className="border border-[#D7D7D7] px-4 py-2 capitalize">
+                {item.state}
+              </td>
+              <td className="border border-[#D7D7D7] px-4 py-2">
+                {item.pincode}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      
+      </table>
+      {itemsPerPage !== studentList.length && (
+        <div className="relative mt-4 flex items-center justify-center gap-4">
+          <Button
+            disabled={currentPage === 1}
+            transparentBtn={'Prev Page'}
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+          />
+          <Button
+            disabled={currentPage === totalPages}
+            transparentBtn={'Next Page'}
+            onClick={() =>
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+            }
+          />
+
+          <p className="text-sm text-[#000000CC]">
+            Page{' '}
+            <span className="mx-2 rounded-[10px] border border-[#00000033] px-5 py-2">
+              {currentPage < 9 && '0'}
+              {currentPage}
+            </span>{' '}
+            of {totalPages < 9 && '0'}
+            {totalPages}
+          </p>
+        </div>
+      )}
+    </div>
   )
 }
 
