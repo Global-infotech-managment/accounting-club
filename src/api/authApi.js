842b38ae467ef2861e7aab87bafdd56a83aefd5d
Sumit-Chouhan-786 <sumitchouhan8221077926@gmail.com>
2025-04-28 10:59:39 +0530

    changes to api url

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,8 +3,12 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  // baseURL: 'https://ec2-51-20-34-228.eu-north-1.compute.amazonaws.com:4000/api/v1',
+  // baseURL: 'http://localhost:4000/api/v1',
   // baseURL: 'https://accountants-club-api.onrender.com/api/v1',
+
+  // sumit render url
+  baseURL: 'https://accountants-club-api-n4z5.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
